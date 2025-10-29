2965816e218c71a95627054554e71a4a847a76d0
NguyenTram05 <tram10102005@gmail.com>
2025-10-29 09:10:35 +0700

    Update product_page.js

--- product_page.js
+++ product_page.js
@@ -3,12 +3,12 @@ function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     sidebar.classList.toggle("active");
 }
-//showmore
-document.addEventListener("DOMContentLoaded", () => {
-    const showmore = document.querySelectorAll(".show_more");
 
-    showmore.forEach(button => {
-        button.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", function() {
+    //showmore
+    const showmore = document.querySelectorAll(".show_more");
+    showmore.forEach(function(button) {
+        button.addEventListener("click", function() {
             const section = button.closest(".product_section");
             const moreProducts = section.querySelector(".more_prod");
             if (moreProducts) {
@@ -17,26 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
-});
-//filter
-document.addEventListener("DOMContentLoaded", () => {
+
+    //filter
     const filterBtn = document.getElementById("filter_btn");
     const filterMenu = document.querySelector(".filter_menu");
     const filterButton = document.querySelectorAll(".filter_button button");
 
-    filterBtn.addEventListener("click", (e) => {
+    filterBtn.addEventListener("click", function(e) {
         e.stopPropagation();
         filterMenu.classList.toggle("active");
     });
 
-    document.addEventListener("click", (e) => {
+    document.addEventListener("click", function(e) {
         if (!filterMenu.contains(e.target) && !filterBtn.contains(e.target)) {
             filterMenu.classList.remove("active");
         }
     });
 
-    filterButton.forEach(btn => {
-        btn.addEventListener("click", () =>
-        filterMenu.classList.remove("active"))
+    filterButton.forEach(function(btn) {
+        btn.addEventListener("click", function(){
+        filterMenu.classList.remove("active");
+        });
     });
 });
