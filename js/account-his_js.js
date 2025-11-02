59eb3c85aeb77a24c6ba92ca613d24269d0ea18e
Tran Phi Quyen <phiquyentran28@gmail.com>
2025-11-02 13:03:36 +0700

    add other tab (same structure w delivered tab)

---
+++ js/account-his_js.js
@@ -0,0 +1,11 @@
+const deliveredTab = document.querySelector('#delivered');
+const intransferTab = document.querySelector('#in-transfer');
+const processingTab = document.querySelector('#processing');
+
+const deliveredList = document.querySelector('.delivered-list');
+const intransferList = document.querySelector('.in-transfer');
+const processingList = document.querySelector('.processing');
+
+deliveredTab.addEventListener('click', () => {
+    
+});
