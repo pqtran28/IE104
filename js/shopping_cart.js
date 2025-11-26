8899b98749153fe8ff0dabdac52fc91ba526f7de
Nguyễn Trần Phương Vy <168315553+pvy0411@users.noreply.github.com>
2025-11-26 17:50:28 +0700

    Add files via upload

---
+++ js/shopping_cart.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const parsePrice = (priceText) => parseInt(priceText.replace(/\D/g, ''), 10);
+    const formatPrice = (price) => price.toLocaleString('vi-VN') + ' VND';
+    
+    const getQuantity = (itemEl) => {
+    const qtyTextEl = itemEl.querySelector('.cart-item-details p:first-child');
+    const matches = qtyTextEl?.textContent.match(/(\d+)$/);
+    return matches ? parseInt(matches[1], 10) : 1;
+    };
+
+    const cartItems = document.querySelectorAll('.cart-item');
+    const totalDisplay = document.getElementById('cartTotalDisplay');
+    
+    
+    const updateCartTotal = () => {
+        let total = 0;
+
+        cartItems.forEach(item => {
+            const priceEl = item.querySelector('.item-price');
+            const select = item.querySelector('.size-select');
+            
+            if (priceEl && select) {
+                const selectedOption = select.options[select.selectedIndex];
+                const basePrice = parsePrice(selectedOption.getAttribute('data-price-base'));
+                
+                const qty = getQuantity(item); 
+                const itemTotal = basePrice * qty;
+                
+                priceEl.textContent = `Thành tiền: ${formatPrice(itemTotal)}`;
+                
+                total += itemTotal;
+            }
+        });
+        
+        if (totalDisplay) {
+            totalDisplay.textContent = formatPrice(total);
+        }
+    };
+    
+    cartItems.forEach(item => {
+        const select = item.querySelector('.size-select');
+        
+        if (select) {
+            select.addEventListener('change', updateCartTotal);
+        }
+    });
+
+    updateCartTotal(); 
+});
